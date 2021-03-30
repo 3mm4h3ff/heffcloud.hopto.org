60d035884be5a705889b8a2cd7771ae50ca12d70
EC2 Default User <ec2-user@ip-172-31-1-14.eu-west-1.compute.internal>
2021-03-30 22:52:20 +0000

    Backup

---
+++ backup/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$con = mysqli_connect("localhost","emma-user","DuckieS0uth!2","register");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
